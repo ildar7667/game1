b2b0bea4a307d0bba558265432fe088ee63bba3f
ildar7554 <i89043727667@gmail.com>
2020-03-19 21:31:23 +0700

    Из главного активити запускается активити авторизация, в контейнер "авторизации" вставляется фрагмент "авторизация"

--- app/src/main/java/com/example/myapplication1/Activity_Auth.kt
+++ app/src/main/java/com/example/myapplication1/Activity_Auth.kt
@@ -2,6 +2,7 @@ package com.example.myapplication1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication1.presentation.starting.AuthorizationFragment
 
 
 class Activity_Auth : AppCompatActivity()  {
@@ -9,5 +10,10 @@ class Activity_Auth : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val ft =
+            supportFragmentManager.beginTransaction()
+         ft.add(R.id.container_auth, AuthorizationFragment())
+         ft.commit()
     }
 }
